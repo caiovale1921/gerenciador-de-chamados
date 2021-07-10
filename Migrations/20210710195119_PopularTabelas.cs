1275153596bc9397daacd026968d8bce90075b1e
caio1 <caio1@LAPTOP-EE98JVOS>
2021-07-10 17:22:47 -0300

    Criação das interfaces de repositorio e adição da migration com dados de teste.

---
+++ Migrations/20210710195119_PopularTabelas.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TiTech.Migrations
+{
+    public partial class PopularTabelas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Clientes VALUES ('Cliente Padrao')");
+            migrationBuilder.Sql("INSERT INTO Funcionarios VALUES ('AdminServer', 'Admin')");
+            migrationBuilder.Sql("INSERT INTO Categorias VALUES ('10/07/2021', 'Manutencao')");
+            migrationBuilder.Sql("INSERT INTO Situacaos VALUES ('10/07/2021', 'Em andamento')");
+            migrationBuilder.Sql("INSERT INTO Prioridades VALUES ('10/07/2021', 'Urgente')");
+            migrationBuilder.Sql("INSERT INTO Chamados VALUES ('Chamado de Teste', '10/07/2021', '1', '1', '1', '1', '1', '10/08/2021', 'Descricao generica do chamado')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM CHAMADO");
+        }
+    }
+}
---
+++ Repositories/IChamadoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiTech.Repositories
+{
+    public interface IChamadoRepository
+    {
+        public IEnumerable<Chamado> Chamados { get; }
+    }
+}
